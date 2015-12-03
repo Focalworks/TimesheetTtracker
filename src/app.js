3e49107b5ea776cdf24c5c89a9e95b40f72f55e3
komal <kom.savla@gmail.com>
2015-12-03 11:48:44 +0530

    Added basic structure

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by komal on 3/12/15.
+ */
+var app = require('app');
+browserWindow = require('browser-window');
+
+var mainWindow = null;
+app.on('ready', function() {
+    mainWindow = new browserWindow({width: 900, height: 700});
+    mainWindow.loadURL("file://" + __dirname + '/windows/main/index.html');
+    //mainWindow.openDevTools();
+});
+
