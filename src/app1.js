3fa8aeff03b3439ecfbf1fef7ea9b9f3367ca7b4
komal <kom.savla@gmail.com>
2015-12-16 10:13:46 +0530

    Working on timesheet form

---
+++ src/app1.js
@@ -0,0 +1,20 @@
+angular
+    .module('MainWindow', ['timer'])
+    .controller('TimerDemoController', ['$scope', function($scope) {
+        $scope.timerRunning = true;
+
+        $scope.startTimer = function (){
+            $scope.$broadcast('timer-start');
+            $scope.timerRunning = true;
+        };
+
+        $scope.stopTimer = function (){
+            $scope.$broadcast('timer-stop');
+            $scope.timerRunning = false;
+        };
+
+        $scope.$on('timer-stopped', function (event, data){
+            console.log('Timer Stopped - data = ', data);
+        });
+
+    }]);
--- src/windows/main/mainController.js
+++ src/windows/main/mainController.js
@@ -2,11 +2,44 @@
  * Created by komal on 3/12/15.
  */
 angular
-    .module('MainWindow', [])
+    .module('MainWindow', ['timer'])
     .controller('MainCtrl', ['$scope', function($scope) {
 
     }]).controller('timesheetCtrl', ['$scope', function($scope) {
         $scope.timesheet = {};
         $scope.timesheet.projectArr = ['Fashion App', 'Sunpharma'];
-        $scope.timesheet.tagArr = ['RND', 'Development', 'PHP'];
+
+        $scope.timesheet.tagArr = {RND: false, Development: false};
+
+        $scope.timerRunning = false;
+
+        $scope.startTimer = function (){
+            $scope.$broadcast('timer-start');
+            $scope.timerRunning = true;
+        };
+
+        $scope.stopTimer = function (){
+            $scope.$broadcast('timer-stop');
+            $scope.timerRunning = false;
+        };
+
+        $scope.response = [];
+        $scope.$on('timer-stopped', function (event, data){
+            var response = {};
+            response.description = $scope.timesheet.description;
+            response.project = $scope.timesheet.project;
+
+            var total_hrs = (data.hours) ? data.hours  + 'h ': '';
+            var total_min = (data.minutes) ? data.minutes + 'm ': '';
+            var total_sec = (data.seconds) ? data.seconds + 's ': '';
+
+            var total_time = total_hrs+total_min+total_sec;
+            response.time = total_time;
+
+            $scope.response.push(response);
+
+
+            console.log('Timer Stopped - data = ', data);
+        });
+
     }]);
